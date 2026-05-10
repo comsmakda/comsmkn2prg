f9fdc4b8cb98ac3ebcf40f0525a4e1663a0f1541
Ikhsan <pratamaikhsan974.com>
2026-05-10 18:36:52 +0800

    update pass db

--- config/database.php
+++ config/database.php
@@ -4,5 +4,5 @@
 define('DB_HOST', 'zlhc21gurbp15ce2pyx1b9um');
 define('DB_NAME', 'comsmakda');
 define('DB_USER', 'comsmakda_user');
-define('DB_PASS', 'COmSmakda@2025!');
+define('DB_PASS', 'C0mSmakda@2025!');
 define('DB_CHARSET', 'utf8mb4');
